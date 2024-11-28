26467e5b84df0704a484d6cbd7f65928148ec3b5
Ignasius Tio <150216840+ignatio001@users.noreply.github.com>
2024-11-28 11:34:52 +0700

    Update Halaman Scan
    Update fitur Scan Fragment

--- app/src/main/java/com/bangkit/catloris/ui/scan/ScanFragment.kt
+++ app/src/main/java/com/bangkit/catloris/ui/scan/ScanFragment.kt
@@ -1,14 +1,24 @@
 package com.bangkit.catloris.ui.scan
 
+import android.net.Uri
 import androidx.fragment.app.viewModels
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import com.bangkit.catloris.R
+import com.bangkit.catloris.databinding.FragmentScanBinding
+import com.bangkit.catloris.utils.getImageUri
 
 class ScanFragment : Fragment() {
+
+    private var _binding: FragmentScanBinding? = null
+    private val binding get() = _binding!!
+    private var currentImageUri: Uri? = null
 
     companion object {
         fun newInstance() = ScanFragment()
@@ -26,6 +36,54 @@ class ScanFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_scan, container, false)
+        _binding = FragmentScanBinding.inflate(inflater, container, false)
+
+        binding.galleryButton.setOnClickListener { startGallery() }
+        binding.cameraButton.setOnClickListener { startCamera() }
+
+        binding.clearPictButton.setOnClickListener { clearPicture() }
+
+        return binding.root
+    }
+
+    private fun clearPicture() {
+        binding.photoAnalyze.setImageDrawable(null)
+    }
+
+    private fun startCamera() {
+        currentImageUri = getImageUri(requireContext())
+        launcherIntentCamera.launch(currentImageUri!!)
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) { isSuccess ->
+        if (isSuccess) {
+            showImage()
+        } else {
+            currentImageUri = null
+        }
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            Log.d("Photo Picker", "No media selected")
+        }
+    }
+
+    private fun showImage() {
+        currentImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.photoAnalyze.setImageURI(it)
+        }
     }
 }
