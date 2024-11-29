3723c0d2913911d4ba85c5c26e41ba4c92516e14
Ignasius Tio <150216840+ignatio001@users.noreply.github.com>
2024-11-29 22:04:32 +0700

    update animate login
    Update Animate Login page

--- app/src/main/java/com/bangkit/catloris/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/bangkit/catloris/ui/auth/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.bangkit.catloris.ui.auth
 
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -31,5 +33,15 @@ class LoginActivity : AppCompatActivity() {
             startActivity(regIntent)
             this.finish()
         }
+
+        playAnimation()
+    }
+
+    private fun playAnimation() {
+        ObjectAnimator.ofFloat(binding.loginImg, View.TRANSLATION_X, -30f, 30f).apply {
+            duration = 7000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
     }
 }
